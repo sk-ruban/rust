afe99a06310999e99f840776f1e362265e5da2ec
Ruban <65346010+sk-ruban@users.noreply.github.com>
2023-10-07 02:50:19 +0800

    Add TRPL Chapter 1 & 2

--- hello_cargo/target/debug/deps/hello_cargo-29283587a7ef4cad.d
+++
@@ -1,5 +0,0 @@
-/Users/ruban/Developer/GitHub/rust/hello_cargo/target/debug/deps/hello_cargo-29283587a7ef4cad: src/main.rs
-
-/Users/ruban/Developer/GitHub/rust/hello_cargo/target/debug/deps/hello_cargo-29283587a7ef4cad.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/ruban/Developer/GitHub/rust/hello_cargo/target/debug/hello_cargo: /Users/ruban/Developer/GitHub/rust/hello_cargo/src/main.rs
