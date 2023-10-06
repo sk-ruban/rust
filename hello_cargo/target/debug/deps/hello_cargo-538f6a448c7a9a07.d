c1cf464bcaea81cdeb751942bea7087d6190abc9
Ruban <65346010+sk-ruban@users.noreply.github.com>
2023-10-07 02:50:19 +0800

    Files removed from .gitignore

--- hello_cargo/target/debug/deps/hello_cargo-538f6a448c7a9a07.d
+++
@@ -1,5 +0,0 @@
-/Users/ruban/Developer/GitHub/rust/hello_cargo/target/debug/deps/hello_cargo-538f6a448c7a9a07.rmeta: src/main.rs
-
-/Users/ruban/Developer/GitHub/rust/hello_cargo/target/debug/deps/hello_cargo-538f6a448c7a9a07.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/release/deps/hello_cargo-e6454f056ad3907e.d
+++
@@ -1,5 +0,0 @@
-/Users/ruban/Developer/GitHub/rust/hello_cargo/target/release/deps/hello_cargo-e6454f056ad3907e: src/main.rs
-
-/Users/ruban/Developer/GitHub/rust/hello_cargo/target/release/deps/hello_cargo-e6454f056ad3907e.d: src/main.rs
-
-src/main.rs:
