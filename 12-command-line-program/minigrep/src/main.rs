2008ed82a55aac048f40f0db7c86ba18d96e22c0
Ruban <65346010+sk-ruban@users.noreply.github.com>
2023-10-28 01:47:22 +0800

    Add TRPL Chapter 12 - Test Driven Development

--- 12-command-line-program/minigrep/src/main.rs
+++ 12-command-line-program/minigrep/src/main.rs
@@ -12,15 +12,10 @@ fn main() {
     // dbg!(args); // print vector
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
         process::exit(1); // will stop the function immediately
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
         process::exit(1);
     }
 }
