b34bae8fa37990655a5021c6ed7999120e1adfca
Ruban <65346010+sk-ruban@users.noreply.github.com>
2023-10-28 02:09:22 +0800

    Add TRPL Chapter 12 - Environment Variables + Standard Error Messages

--- 12-command-line-program/minigrep/src/main.rs
+++ 12-command-line-program/minigrep/src/main.rs
@@ -12,10 +12,12 @@ fn main() {
     // dbg!(args); // print vector
 
     let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}"); // prints into the std error stream
         process::exit(1); // will stop the function immediately
     });
 
     if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
