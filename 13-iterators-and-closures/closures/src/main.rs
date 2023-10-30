2df44cdbc235d712cb59d815719d68ee531e807c
Ruban <65346010+sk-ruban@users.noreply.github.com>
2023-10-30 21:18:27 +0800

    Add TRPL Chapter 13 - Closures

---
+++ 13-iterators-and-closures/closures/src/main.rs
@@ -0,0 +1,62 @@
+//  converting a function into a closure
+// fn  add_one_v1   (x: u32) -> u32 { x + 1 }
+// let add_one_v2 = |x: u32| -> u32 { x + 1 };
+// let add_one_v3 = |x|             { x + 1 };
+// let add_one_v4 = |x|               x + 1  ;
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
+        }
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
+}
+
+fn main() {
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
+}
+
+// FnOnce - Closures that move captured values out of body and can be called once
+// FnMut - Closures that don't move captured values out of body but might mutate values
+// Fn - Can be called more than once.
