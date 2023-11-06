878c9a63544b4b9495ffbd302547a95ec80bd4f9
Ruban <65346010+sk-ruban@users.noreply.github.com>
2023-11-06 08:38:44 +0800

    Add TRPL Chapter 19 - Advanced Traits

---
+++ 19-advanced-features/advanced-traits/src/main.rs
@@ -0,0 +1,52 @@
+use std::ops::Add;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+fn main() {
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
+
+    println!("A baby dog is called a {}", Dog::baby_name());
+    // A baby dog is called a Spot
+
+    // println!("A baby dog is called a {}", Animal::baby_name());
+    // Does not work since Animal::baby_name does not have a self parameter
+
+    println!("A baby dog is called a {}", <Dog as Animal>::baby_name());
+    // A baby dog is called a puppy
+}
+
+trait Animal {
+    fn baby_name() -> String;
+}
+
+struct Dog;
+
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Spot")
+    }
+}
+
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
+    }
+}
